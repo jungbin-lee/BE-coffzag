55ab34a52d47d2a12d1848408615cff665c507c0
eungenie <pkhykk@naver.com>
2021-04-12 21:35:48 +0900

    Create coffzag directory

---
+++ BE-coffzag/src/main/java/com/mini/coffzag/CoffzagApplication.java
@@ -0,0 +1,13 @@
+package com.mini.coffzag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffzagApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoffzagApplication.class, args);
+    }
+
+}
