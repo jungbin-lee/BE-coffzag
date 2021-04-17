22a3bacf8ec3b5fb92542e5357063cb82d92a415
eungenie <pkhykk@naver.com>
2021-04-17 14:06:47 +0900

    Modify Review GenerationType IDENTITY -> AUTO

--- BE-coffzag/src/main/java/com/mini/coffzag/entity/Review.java
+++ BE-coffzag/src/main/java/com/mini/coffzag/entity/Review.java
@@ -14,7 +14,7 @@ import javax.persistence.*;
 @Entity
 public class Review extends Timestamped{
 
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     @Id
     @Column(name = "review_id")
     private Long reviewId;
